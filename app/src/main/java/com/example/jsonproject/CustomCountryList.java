e077240c9fcc7a42c4daf6053b6552756ccbb0bb
asmarokadiya <asmarokadiya7@gmail.com>
2019-10-19 16:17:42 -0400

    final commit

--- app/src/main/java/com/example/jsonproject/CustomCountryList.java
+++ app/src/main/java/com/example/jsonproject/CustomCountryList.java
@@ -1,9 +1,11 @@
 package com.example.jsonproject;
 
 import android.app.Activity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -17,23 +19,54 @@ public class CustomCountryList extends BaseAdapter {
         this.countries = countries;
     }
 
+    static class ViewHolder{
+
+        TextView textviewId;
+        TextView textViewCountry;
+
+    }
+
     @Override
     public int getCount() {
-        return 0;
+       if(countries.size()<=0){
+           return 1;
+       }
+       return countries.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return position;
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+
+        View row=convertView;
+        LayoutInflater inflater=context.getLayoutInflater();
+        ViewHolder vh;
+
+        if (convertView==null){
+
+            vh=new ViewHolder();
+            row=inflater.inflate(R.layout.rowi_tem,null,true);
+            vh.textviewId=(TextView)row.findViewById(R.id.textviewID1);
+            vh.textViewCountry=(TextView)row.findViewById(R.id.textviewID2);
+
+            row.setTag(vh);
+
+        }
+        else {
+            vh=(ViewHolder)convertView.getTag();
+
+        }
+        vh.textViewCountry.setText(countries.get(position).getName());
+        vh.textviewId.setText(""+countries.get(position).getId());
+        return row;
     }
 }
--- app/src/main/java/com/example/jsonproject/MainActivity.java
+++ app/src/main/java/com/example/jsonproject/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,10 +28,11 @@ public class MainActivity extends AppCompatActivity {
     StringBuffer response;
     Activity activity;
     URL url;
+    ArrayList<country>countries=new ArrayList<country>();
     ProgressDialog progressDialog;
     ListView listView;
 
-    String path="https://api.myjson.com/bins/13wavw";
+    String path="https://api.myjson.com/bins/8rjnw";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +41,79 @@ public class MainActivity extends AppCompatActivity {
 
         btnsubmit=(Button) findViewById(R.id.btn);
         listView=(ListView)findViewById(R.id.list1);
+        btnsubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 countries.clear();
+                 new GetData().execute();
+            }
+        });
+
+    }
+    class GetData extends AsyncTask{
+
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+        }
+
+
+            @Override
+        protected Object doInBackground(Object[] objects) {
+            return getWebservicecalls();
+        }
+
+        private Object getWebservicecalls() {
+            try {
+
+                url=new URL(path);
+                HttpURLConnection connection=(HttpURLConnection)url.openConnection();
+                connection.setRequestMethod("GET");
+                int responsecode=connection.getResponseCode();
+                if (responsecode == HttpURLConnection.HTTP_OK) {
+                    BufferedReader in = new
+                            BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    String output;
+
+                    response = new StringBuffer();
+
+                    while ((output = in.readLine()) != null) {
+
+                        response.append(output);
+
+                    }
+                    in.close();
+
+
+                }
+            }
+            catch (Exception e){e.printStackTrace();}
+            responseText=response.toString();
+
+            try {
+                JSONArray jsonArray=new JSONArray(responseText);
+                for (int i=0; i<jsonArray.length();i++){
+
+                    JSONObject jsonObject=jsonArray.getJSONObject(i);
+                    int id=jsonObject.getInt("id");
+                    String country=jsonObject.getString("CountryName");
+                    country countryobj=new country(country, id);
+                    countries.add(countryobj);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            super.onPostExecute(o);
+            CustomCountryList customCountryList=new CustomCountryList(activity,countries);
+            listView.setAdapter(customCountryList);
+        }
+
 
     }
 }
+
